669b466e059101c66d1002eeb93883691941b6fb
dk460110 <dk460110@gmail.com>
2021-02-04 00:01:49 -0500

    test script

--- buy_signal.py
+++ buy_signal.py
@@ -52,6 +52,29 @@ def get_signal_prices(candle_minutes, symbols):
 
     return df_p_hist
 
+df_max = df[df['Date'].isin(pd.Series(df['Date'].unique()).nlargest(2))].reset_index(drop=True)
+
+test = ['NIO']
+results = []
+
+for symbol in df_max['Symbol'].unique():
+    df_one = df_max[df_max['Symbol'] == symbol]
+
+    try:
+        if ((df_one.iloc[-2]['Open'] < df_one.iloc[-2]['SMA_9']) and
+                (df_one.iloc[-2]['Close'] < df_one.iloc[-2]['SMA_9']) and
+                (df_one.iloc[-1]['Open'] < df_one.iloc[-1]['SMA_9']) and
+                (df_one.iloc[-1]['Close'] > df_one.iloc[-1]['SMA_9'])):
+            results.append([symbol, True])
+
+        else:
+            results.append([symbol, False])
+
+    except:
+        next
+
+text_df = pd.DataFrame(results, columns=['Ticker', 'Text'])
+
 client = boto3.client(
     "sns",
     aws_access_key_id = config.python_texter_ak,
@@ -59,4 +82,7 @@ client = boto3.client(
     region_name = config.python_texter_region
 )
 
-client.publish(PhoneNumber = "", Message = "{} ready for trade".format(Ticker))
+for i in range(0,len(text_df)):
+    if text_df['Text'][i] == True:
+        client.publish(PhoneNumber = "",
+                       Message = "{} is now set up for a day trade on the 5-minute chart.".format(text_df['Ticker'][i]))
